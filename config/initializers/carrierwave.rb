5ce7aa27f59f75a5422245f1e994340c553b9be1
tkendalls <director@mathplusacademy.com>
2013-06-12 21:18:02 -0400

    Add env vars for HEROKU

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIL65VEYP6WCPNOGQ',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_access_key_id      => ENV['S3_KEY'],                # required
+    :aws_secret_access_key  => ENV['S3_SECRET']              # required
     :region                 => 'us-east-1',                  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'MPAAppImages'                     # required
+  config.fog_directory  = ENV['S3_BUCKET']                     # required
 end
