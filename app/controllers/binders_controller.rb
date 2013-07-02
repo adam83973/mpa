3775ec4b809e1795b4662e4ecec1773917659070
tkendalls <director@mathplusacademy.com>
2013-07-01 21:37:21 -0400

    Add ability to print bindercover for middleschool classes and brainbuilders

--- app/controllers/binders_controller.rb
+++ app/controllers/binders_controller.rb
@@ -1,11 +1,20 @@
 class BindersController < ActionController::Base
 
-  def print
+  def briefcase
     @student = Student.find(params[:student])
 
     if @student.offerings
       @offerings = @student.offerings.where("course_id < ?", 7)
       @first_offering = @offerings.first
+    end
+  end
+
+  def middleschool
+    @student = Student.find(params[:student])
+
+    if @student.offerings
+      @offerings = @student.offerings.where("course_id > ?", 6)
+      @second_offering = @offerings.first
     end
   end
 end
