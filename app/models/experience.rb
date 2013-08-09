808c5a335affad4fd756c67dcad6c8770f17c60a
tkendalls <director@mathplusacademy.com>
2013-08-08 20:40:22 -0400

    update experience points so when grading scores auto populate, add presence validation to name points and comments for experience

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,8 @@
 class Experience < ActiveRecord::Base
   attr_accessible :category, :content, :name, :points, :image, :remove_image
 
+  validates_presence_of :points, :category, :name
+
   has_many :experience_points
 
   mount_uploader :image, ImageUploader
