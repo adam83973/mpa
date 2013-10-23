a6d08a70aa67cb2bcf03b941513f7046c12b88aa
tkendalls <director@mathplusacademy.com>
2013-10-23 09:40:22 -0400

    update to migration

--- db/migrate/20131016215358_activities_lessons.rb
+++ db/migrate/20131016215358_activities_lessons.rb
@@ -2,7 +2,7 @@ class ActivitiesLessons < ActiveRecord::Migration
   def change
     create_table :activities_lessons, :id => false do |t|
       t.integer :activity_id
-      t.integer :lessons
+      t.integer :lesson_id
     end
   end
 end
