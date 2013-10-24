fd99372396bb9d264a7537bba758ca564b4e567b
tkendalls <director@mathplusacademy.com>
2013-10-24 16:51:29 -0400

    lesson commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20131022195700) do
-=======
-ActiveRecord::Schema.define(:version => 20131018142931) do
->>>>>>> activitieslessons
 
   create_table "activities", :force => true do |t|
     t.string   "title"
