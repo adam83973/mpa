af59c980fa813429947f9253d2f032fd74cb253d
tkendalls <director@mathplusacademy.com>
2013-10-30 07:10:02 -0400

    commit

--- db/migrate/20131018142832_remove_lessons_from_activities_lesson.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLessonsFromActivitiesLesson < ActiveRecord::Migration
-  def up
-    remove_column :activities_lessons, :lessons
-  end
-
-  def down
-    add_column :activities_lessons, :lessons, :integer
-  end
-end
