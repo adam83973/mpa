a020aee8e14b4cfd11f939144574958080b68ad0
tkendalls <director@mathplusacademy.com>
2013-11-07 14:13:02 -0500

    update styling in location index

--- app/helpers/offerings_helper.rb
+++ app/helpers/offerings_helper.rb
@@ -1,2 +1,9 @@
 module OfferingsHelper
+  def offering_capacity(offering)
+    if offering.at_capacity?
+      "<span class='label label-important pull-right'>Full</span>"
+    else
+      "<span class='label label-success pull-right'>Open</span>"
+    end
+  end
 end
