0a71e8de0f7c6053c936958810b87e7d793d2754
tkendalls <director@mathplusacademy.com>
2013-11-14 12:54:17 -0500

    update to migration file

--- db/migrate/20131028221902_add_last_payment_text_to_users.rb
+++ db/migrate/20131028221902_add_last_payment_text_to_users.rb
@@ -1,7 +1,5 @@
 class AddLastPaymentTextToUsers < ActiveRecord::Migration
   def change
-    unless column_exists? :users, :last_payment
-      add_column :users, :last_payment, :text
-    end
+    add_column :users, :last_payment, :text
   end
 end
