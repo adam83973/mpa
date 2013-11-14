c8eddc8a0b09587eaf4a30b1bdfe68ae44523265
tkendalls <director@mathplusacademy.com>
2013-11-14 13:10:59 -0500

    update last_payment migration

--- db/migrate/20131028192422_remove_last_payment_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLastPaymentFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :last_payment
-  end
-
-  def down
-    add_column :users, :last_payment, :string
-  end
-end
--- db/migrate/20131028221902_add_last_payment_text_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLastPaymentTextToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :last_payment, :text
-  end
-end
