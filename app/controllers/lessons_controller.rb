93cb6b231d9cafe3348382a4aa9f24bebec42396
tkendalls <director@mathplusacademy.com>
2013-12-16 10:36:37 -0500

    update to lesson controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -5,7 +5,7 @@ class LessonsController < ApplicationController
   # GET /lessons
   # GET /lessons.json
   def index
-    @lessons = Lesson.includes(:standard)
+    @lessons = Lesson.includes(:standard).order(id: :asc)
 
     respond_to do |format|
       format.html # index.html.erb
