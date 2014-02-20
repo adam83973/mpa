74e88276a1941deb8b51a13c2809598cba3af871
tkendalls <director@mathplusacademy.com>
2014-02-20 11:19:50 -0500

    update

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,5 +1,5 @@
 class Lead < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :phone, :stage_id, :student_information, :user_id
+  attr_accessible :email, :first_name, :last_name, :phone, :stage_id, :student_information, :user_id, :active
 
   belongs_to :user
   belongs_to :stage
@@ -12,5 +12,9 @@ class Lead < ActiveRecord::Base
 
   def update_stage(id)
     update_column(:stage_id, id.to_i)
+  end
+
+  def self.active_stage_count(stage_name, user_id)
+    self.where('stage_id = ? AND user_id = ? AND active = ?', Stage.where('name = ?', stage_name), user_id, true).count
   end
 end
