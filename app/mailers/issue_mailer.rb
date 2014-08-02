144e60cf994320abd16973920779e1e829e1d43d
tkendalls <director@mathplusacademy.com>
2014-08-02 12:19:00 -0400

    issue mailer < admin mailer

---
+++ app/mailers/issue_mailer.rb
@@ -0,0 +1,36 @@
+class IssueMailer < AdminMailer
+  default from: "info@mathplusacademy.com"
+
+  def return_from_hold(student)
+    @user = student.user
+    @student = student
+
+    mail(
+      to: @user.email
+    )
+  end
+
+  def issue_submission_notification(issue)
+    @issue = issue
+    @user = User.find(issue.user_id)
+    mail(
+      to: @user.email
+    )
+  end
+
+  def issue_notification(issue)
+    @issue = issue
+    @user = User.find(issue.user_id)
+    mail(
+      to: "director@mathplusacademy.com"
+    )
+  end
+
+  def issue_closed_notification(issue)
+    @issue = issue
+    @user = User.find(issue.user_id)
+    mail(
+      to: @user.email
+    )
+  end
+end
