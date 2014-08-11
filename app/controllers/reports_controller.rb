d2e822eba4331ddc92933202f89144b77f7b8d07
tkendalls <director@mathplusacademy.com>
2014-08-11 16:35:52 -0400

    update

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,17 +5,17 @@ class ReportsController < ApplicationController
 
   def show
     @params = params[:query]
-    if params[:query][:status] =! ''
-      if params[:query][:start_date_range] && params[:query][:end_date_range]
+    if params[:query][:student_status] =! ''
+      if params[:query][:start_date_range] != '' && params[:query][:end_date_range] != ''
         @students = Student.where("
           #{Report::TYPE[params[:query][:type].to_i].downcase.tr(' ', '_')} >= ? AND
           #{Report::TYPE[params[:query][:type].to_i].downcase.tr(' ', '_')} <= ? AND
           status = ?",
           Date.parse(params[:query][:start_date_range]),
           Date.parse(params[:query][:end_date_range]),
-          params[:query][:status])
+          Student::STATUSES[params[:query][:student_status].to_i])
       else
-        @students = Student.where(:status => params[:query][:status])
+        @students = Student.where(:status => params[:query][:student_status])
       end
     else
       @students = Student.where("
