2e6d8c6fcee99f63c42c20dec401ff5b7e08654c
tkendalls <director@mathplusacademy.com>
2014-08-11 12:20:35 -0400

    update

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,14 +5,17 @@ class ReportsController < ApplicationController
 
   def show
     @params = params[:query]
-    if params[:query][:status]
-      @students = Student.where("
-        #{Report::TYPE[params[:query][:type].to_i].downcase.tr(' ', '_')} >= ? AND
-        #{Report::TYPE[params[:query][:type].to_i].downcase.tr(' ', '_')} <= ? AND
-        status = ?",
-        Date.parse(params[:query][:start_date_range]),
-        Date.parse(params[:query][:end_date_range]),
-        params[:query][:status])
+    if params[:query][:status] =! ''
+      if params[:query][:start_date_range] && params[:query][:end_date_range]
+        @students = Student.where("
+          #{Report::TYPE[params[:query][:type].to_i].downcase.tr(' ', '_')} >= ? AND
+          #{Report::TYPE[params[:query][:type].to_i].downcase.tr(' ', '_')} <= ? AND
+          status = ?",
+          Date.parse(params[:query][:start_date_range]),
+          Date.parse(params[:query][:end_date_range]),
+          params[:query][:status])
+      else
+        @students = Student.where
     else
       @students = Student.where("
         #{Report::TYPE[params[:query][:type].to_i].downcase.tr(' ', '_')} >= ? AND
