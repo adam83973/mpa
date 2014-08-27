fa8d119684155a144f17841758ec6d618d944fe8
tkendalls <director@mathplusacademy.com>
2014-08-27 12:01:03 -0400

    add lead to navbar search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -71,6 +71,7 @@ class StaticPagesController < ApplicationController
       @users_search = User.search(params[:search])
       @offerings_search = Offering.search(params[:search])
       @students_search = Student.search(params[:search])
+      @leads_search = Lead.search(params[:search])
     end
   end
 
