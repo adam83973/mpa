61130d9d30d46bd89e1b19cd6e0f38ef255c79f3
tkendalls <director@mathplusacademy.com>
2014-08-28 15:49:35 -0400

    udpate

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -4,7 +4,7 @@ class FileUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility
   include Sprockets::Helpers::RailsHelper
@@ -54,7 +54,7 @@ class FileUploader < CarrierWave::Uploader::Base
     self.file.instance_variable_set(:@content_type, "image/jpeg")
   end
 
-  def cover 
+  def cover
     manipulate! do |frame, index|
       frame if index.zero?
     end
