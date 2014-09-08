2382faedefa0dddb83925ec2a9445a56b0fa037e
tkendalls <director@mathplusacademy.com>
2014-09-07 21:50:14 -0400

    Infusionsoft sandbox implementation

--- config/initializers/infusionsoft.rb
+++ config/initializers/infusionsoft.rb
@@ -1,5 +1,11 @@
 Infusionsoft.configure do |config|
-  config.api_url = 'jmrmath.infusionsoft.com' # example infused.infusionsoft.com
-  config.api_key = '7ffb0735b825e206bc4bf9e50198af6c'
-  config.api_logger = Logger.new("#{Rails.root}/log/infusionsoft_api.log") # optional logger file
+  if Rails.env.production?
+    config.api_url = 'jmrmath.infusionsoft.com' # example infused.infusionsoft.com
+    config.api_key = ENV['INFUSIONSOFT_KEY']
+    config.api_logger = Logger.new("#{Rails.root}/log/infusionsoft_api.log") # optional logger file
+  else
+    config.api_url = 'po206.infusionsoft.com' # example infused.infusionsoft.com
+    config.api_key = ENV['INFUSIONSOFT_SANDBOX_KEY']
+    config.api_logger = Logger.new("#{Rails.root}/log/infusionsoft_sandbox.log") # optional logger file
+  end
 end
