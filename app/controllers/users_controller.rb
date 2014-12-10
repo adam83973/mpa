9668f4a2178e261fa22ca03231edbfe0f93073ac
tkendalls <director@mathplusacademy.com>
2014-12-09 20:18:52 -0500

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -318,7 +318,7 @@ class UsersController < ApplicationController
 
   def appointment_request
     req = JSON.parse(request.body.read)
-    puts request["calendarid"]
+    puts req["calendarid"]
     render nothing: true
   end
 end
