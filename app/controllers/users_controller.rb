84feeefdc396bd16c8a593a1328f012689452fe0
tkendalls <director@mathplusacademy.com>
2014-12-19 16:50:57 -0500

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -433,12 +433,12 @@ class UsersController < ApplicationController
       # create note
       @note = @user.notes.build({content: "#{@user.full_name} has scheduled an appointment. Please create an opportunity.", user_id: User.find_by_email("system@mathplusacademy.com").id, location_id: @user.location_id, action_date: Date.today})
 
-      @note = @note.save
-
-      puts "#{@user.full_name} added."
-      puts "Appointment #{@appointment.id} added."
-      puts "Note #{@note.id} added."
-      puts appointment
+        if @note = @note.save
+          puts "#{@user.full_name} added."
+          puts "Appointment #{@appointment.id} added."
+          puts "Note #{@note.id} added."
+          puts appointment
+        end
     end
 
     render nothing: true
