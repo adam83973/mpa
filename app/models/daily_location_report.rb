4bf04e28342953c9ba9a38c928d0aeace9d050d7
tkendalls <director@mathplusacademy.com>
2015-03-04 14:48:37 -0500

    update

--- app/models/daily_location_report.rb
+++ app/models/daily_location_report.rb
@@ -2,4 +2,13 @@ class DailyLocationReport < ActiveRecord::Base
   attr_accessible :add_count, :drop_count, :location_id, :parent_logins, :total_enrollment
 
   belongs_to :location
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |report|
+        csv << report.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
