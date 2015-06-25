d492c0c314c9312679deaeeb8e487ff46bea7acb
tkendalls <director@mathplusacademy.com>
2015-06-25 13:29:57 -0400

    update

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -55,7 +55,7 @@ class Registration < ActiveRecord::Base
   def self.to_csv
     CSV.generate do |csv|
       csv << column_names
-      all.each do |student|
+      all.each do |registration|
         csv << registration.attributes.values_at(*column_names)
       end
     end
