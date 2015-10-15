ea36178a4c2095f804315ffd17bf310e3207f2ed
tkendalls <director@mathplusacademy.com>
2015-10-15 14:59:07 -0400

    udpat

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,7 @@
 class CoursesController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :authorize_admin, except: :show
+  before_filter :authorize_employee, except: [:show]
+  before_filter :authorize_admin, except: [:show, :index]
 
 
 
