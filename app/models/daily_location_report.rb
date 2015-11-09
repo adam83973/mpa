ab906b7e2e4981dabaa77e7d9fc52f91e023cc59
tkendalls <director@mathplusacademy.com>
2015-11-09 15:52:22 -0500

    update

--- app/models/daily_location_report.rb
+++ app/models/daily_location_report.rb
@@ -1,6 +1,8 @@
 class DailyLocationReport < ActiveRecord::Base
-  attr_accessible :add_count, :drop_count, :location_id, :parent_logins, :total_enrollment, :hw_help_count, :assessment_count, :opportunities_won_count, :opportunities_lost_count
+  attr_accessible :add_count, :drop_count, :location_id, :parent_logins, :total_enrollment, :hw_help_count, :assessment_count, :opportunities_won_count, :opportunities_lost_count, :offering_information
 
+  serialize :offering_information
+  
   belongs_to :location
 
   def self.to_csv
