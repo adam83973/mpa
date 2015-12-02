50220784ca1e1722f4c19f75a805b0d7b7e4b73b
tkendalls <director@mathplusacademy.com>
2015-12-02 16:36:32 -0500

    update

--- app/helpers/opportunities_helper.rb
+++ app/helpers/opportunities_helper.rb
@@ -34,4 +34,33 @@ module OpportunitiesHelper
       '<i class="fa fa-fire pull-right"></i>'
     end
   end
+
+  #return list of changes.
+  def opportunity_changes(version)
+    content = "<ol> "
+    version.changeset.delete_if{|k,v| k == 'updated_at'}.each do |attribute, changes|
+      if changes[0] == nil
+        if attribute == 'status'
+          content = content + "<li>Status set to <span style='color:green;'>#{changes[1]}</span>.</li>"
+        elsif attribute == 'course_id'
+          content = content + "<li>Course set to <span style='color:green;'>#{Course.find(changes[1]).course_name}</span>.</li>"
+        elsif attribute == 'offering_id'
+          content = content + "<li>Offering set to <span style='color:green;'>#{Offering.find(changes[1]).offering_name}</span>.</li>"
+        else
+          content = content + "<li>#{attribute.gsub('_', ' ').split.map(&:capitalize).join(' ')} <span style='color:green;'>added</span>.</li>"
+        end
+      else
+        if attribute == 'status'
+          content = content + "<li>#{attribute.gsub('_', ' ').split.map(&:capitalize).join(' ')} changed from <span style='color:red;'>#{Opportunity::STATUSES[changes[0]]}</span> to <span style='color:green;'>#{Opportunity::STATUSES[changes[1]]}</span>.</li>"
+        elsif attribute == 'course_id'
+          content = content + "<li>Course changed from <span style='color:red;'>#{Course.find(changes[0]).course_name}</span> to <span style='color:green;'>#{Course.find(changes[1]).course_name}</span>.</li>"
+        elsif attribute == 'offering_id'
+          content = content + "<li>Offering changed from <span style='color:red;'>#{Offering.find(changes[0]).offering_name}</span> to <span style='color:green;'>#{Offering.find(changes[1]).offering_name}</span>.</li>"
+        else
+          content = content + "<li>#{attribute.gsub('_', ' ').split.map(&:capitalize).join(' ')} changed from <span style='color:red;'>#{changes[0]}</span> to <span style='color:green;'>#{changes[1]}</span>.</li>"
+        end
+      end
+    end
+    content = content + "</ol>"
+  end
 end
