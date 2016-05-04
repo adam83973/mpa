fd428bbb5186a7ab9a80fcac105bf08e5bc686c2
tkendalls <director@mathplusacademy.com>
2016-05-04 16:06:43 -0400

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -580,12 +580,12 @@ class UsersController < ApplicationController
     end
 
     def slack_note_content(appointment, app_obj)
-      content = "Number of students: #{appointment['customField1']}\n #{'Student'.pluralize(appointment['customField1'].to_i)}: #{appointment['customField2']} (#{appointment['customField3']})\n"
+      content = "Number of students: #{appointment['customField1']} #{'Student'.pluralize(appointment['customField1'].to_i)}: #{appointment['customField2']} (#{appointment['customField3']})\n"
 
       # add student information to note it there are more than one students
       case appointment['customField1'].to_i
       when 2
-        content = content + ", #{appointment['customField4'] if appointment['customField4']} (#{appointment['customField5'] if appointment['customField5']})\n"
+        content = content + ", #{appointment['customField4'] if appointment['customField4']} (#{appointment['customField5'] if appointment['customField5']})"
       when 3
         content = content + ", #{appointment['customField4'] if appointment['customField4']} (#{appointment['customField5'] if appointment['customField5']}), #{appointment['customField6'] if appointment['customField6']} (#{appointment['customField7'] if appointment['customField7']}) \n"
       else
@@ -594,7 +594,9 @@ class UsersController < ApplicationController
 
       content = content + "Appointment: #{app_obj.time.strftime("%b %d,%l:%M%p")}\n"
 
-      content = content + "Comments: #{appointment['customField9'] ? appointment['customField9'] : "No comments."}"
+      content = content + "Comments: #{appointment['customField9'] ? appointment['customField9'] : "No comments."}\n"
+
+      content = content + "Source: #{appointment['customField10'] ? appointment['customField10'] : "No source."}\n"
 
       content
     end
