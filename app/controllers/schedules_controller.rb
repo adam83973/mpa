6b724a065c55f010b43f9569135399b9610f6e1b
tkendalls <director@mathplusacademy.com>
2016-07-14 13:36:16 -0400

    update

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,16 +1,16 @@
 class SchedulesController < ActionController::Base
   def powell
     @location = Location.includes(:offerings).find(1)
-    @offerings = @location.offerings
+    @offerings = @location.offerings.includes(:registrations)
   end
 
   def new_albany
     @location = Location.includes(:offerings).find(2)
-    @offerings = @location.offerings
+    @offerings = @location.offerings.includes(:registrations)
   end
 
   def mill_run
     @location = Location.includes(:offerings).find(3)
-    @offerings = @location.offerings
+    @offerings = @location.offerings.includes(:registrations)
   end
 end
