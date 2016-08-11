cc57e50ed927ceed0cc0a8423746a0f89f467124
tkendalls <director@mathplusacademy.com>
2016-08-11 16:45:15 -0400

    update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -36,7 +36,7 @@ class StaticPagesController < ApplicationController
   def events
     woocommerce = set_woo
 
-    @workshops = woocommerce.get('products', {filter: {category: 'jump-start', limit: 20} }).parsed_response['products']
+    @workshops = woocommerce.get('products', {filter: {category: 'workshop', limit: 20} }).parsed_response['products']
   end
 
   def event_enrollment
