a366614721b1537b0e19ec173bc9008e199721e4
tkendalls <director@mathplusacademy.com>
2016-08-18 12:14:14 -0400

    update

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -2,11 +2,11 @@ module AttendancesHelper
   def last_attendance_date(attendance)
     date = attendance
     if date > Date.today - 14.days == true
-      "<span style='color:green;'>#{@last_attendance.date.strftime("%D")}</span>".html_safe
+      "<span style='color:green;'>#{date.strftime("%D")}</span>".html_safe
     elsif date <= Date.today - 14.days && date >= Date.today - 30.days == true
-      "<span style='color:orange;'>#{@last_attendance.date.strftime("%D")}</span>".html_safe
+      "<span style='color:orange;'>#{date.strftime("%D")}</span>".html_safe
     elsif date < Date.today - 30.days == true
-      "<span style='color:red;'>#{@last_attendance.date.strftime("%D")}</span>".html_safe
+      "<span style='color:red;'>#{date.strftime("%D")}</span>".html_safe
     else
       "<span>No Attendance</span>"
     end
