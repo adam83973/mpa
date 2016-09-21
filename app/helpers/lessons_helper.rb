d5aeaeccba932be415df98aa438e9f7df9fdb66c
tkendalls <director@mathplusacademy.com>
2016-09-21 11:45:50 -0400

    update

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -2,4 +2,12 @@ module LessonsHelper
   def resource_lookup(id)
     Resource.find(id) unless id == nil || id == ''
   end
+
+  def lesson_status(lesson)
+    if lesson.contains_error
+      "<span class='label label-danger'>Needs Review</span>".html_safe
+    else
+      "<span class='label label-success'>OK</span>".html_safe
+    end
+  end
 end
