f5913dcbfb30ecffb7fd1d2458c96a06633610f3
tkendalls <director@mathplusacademy.com>
2016-10-26 10:52:13 -0400

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -573,7 +573,8 @@ class UsersController < ApplicationController
 
     message_type = request.headers["x-amz-sns-message-type"]
     puts "Message Type: #{message_type}"
-    puts "Entire request \n ############################################# \n" + request.env
+    puts "Entire request \n ############################################# \n"
+    request.env.each{|k, v| puts k + ": " v + "\n"}
 
     response = request.body.read
     if message_type == "SubscriptionConfirmation"
