31adf0964c33fe8e2204f8016fccf3c4db278581
tkendalls <director@mathplusacademy.com>
2017-04-26 15:57:25 -0400

    update

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,18 @@
 class SchedulesController < ActionController::Base
+  around_action :scope_current_company
+  
+  def current_company
+    @current_company ||= Company.find_by_subdomain(request.subdomain)
+  end
+
+  def scope_current_company(&block)
+    if current_company.nil?
+      yield
+    else
+      current_company.scope_schema("public", &block)
+    end
+  end
+
   def powell
     @location = Location.includes(:offerings).find(1)
     @offerings = @location.offerings.includes(:registrations)
