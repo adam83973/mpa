937cd59a9f684ff93dc759c4113a8c31600fba2c
tkendalls <director@mathplusacademy.com>
2017-04-28 19:58:49 -0400

    update

--- app/controllers/binders_controller.rb
+++ app/controllers/binders_controller.rb
@@ -1,14 +1,27 @@
 class BindersController < ActionController::Base
   before_action :authenticate_user!
   before_action :authorize_employee
+  around_action :scope_current_company
+
+  def current_company
+    @current_company ||= Company.find_by_subdomain(request.subdomain)
+  end
+
+  def scope_current_company(&block)
+    if current_company.nil?
+      yield
+    else
+      current_company.scope_schema("public", &block)
+    end
+  end
 
   def briefcase
     @student = Student.find(params[:student_id])
     @offering = Offering.find(params[:offering_id])
   end
 
-# Sets instance variable for student offering, used to print binder for
-# middle school and brain builder classes.
+  # Sets instance variable for student offering, used to print binder for
+  # middle school and brain builder classes.
   def middleschool
     @student = Student.find(params[:student_id])
     @offering = Offering.find(params[:offering_id])
