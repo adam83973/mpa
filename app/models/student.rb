63cb2eeb51656759c128586a50a1f418033a58e8
tkendalls <director@mathplusacademy.com>
2017-05-30 11:01:52 -0400

    update

--- app/models/student.rb
+++ app/models/student.rb
@@ -361,7 +361,7 @@ class Student < ActiveRecord::Base
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       student = find_by_id(row["id"]) || new
-      student.attributes = row.to_hash.slice(*accessible_attributes)
+      student.attributes = row.to_hash.slice(*column_names)
       student.save!
     end
   end
